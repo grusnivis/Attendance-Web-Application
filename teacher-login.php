e0be439ba5c09c1c8ffbd04a63ee651217238ba9
grusnivis <kathryn.sigaya@gmail.com>
2022-06-11 01:13:55 +0800

    testing git commands

--- teacher-login.php
+++ teacher-login.php
@@ -1,4 +1,10 @@
 
 <html>
     <title> Teacher Menu </title>
+    <body>
+        <table class = "tablelogin">
+            <td> test test test test </td>
+            <td> test test test test </td>
+        </table>
+    </body>
 </html>
