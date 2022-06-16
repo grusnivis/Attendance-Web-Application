8b49f3d687e12ad2fc4993b01ae3e9c6f7de91a2
grusnivis <kathryn.sigaya@gmail.com>
2022-06-16 23:46:29 +0800

    revamped teacher login page, renamed files

--- teacher-login.php
+++ teacher-login.php
@@ -1,41 +1,88 @@
+
+<?php
+/*
+    error_reporting(E_ALL & ~E_NOTICE);
+    ini_set("display_errors", 1);
+  
+    require_once 'ims-blti/blti.php';
+    $lti = new BLTI("secret", false, false);
+
+    ob_start();
+    session_start();
+    header('Content-Type: text/html; charset=utf-8'); 
+    */
+?>
+
 <html>
     <head>
-    <title> Teacher Menu </title>
-    <link type = "text/css" rel = "stylesheet" href = "teacher-login-style.css"/>
+        <title>Group H - Web Application</title>
+        <link type = "text/css" rel = "stylesheet" href = "css/teacher-login-style.css" />
     </head>
     <body>
-        <table class = "tablelogin">
-            <tr>
-                <th>
-                    <!-- temporary icon pls don't judge -->
-                    </br>
-                    <div class = "logo" >
-                        <img src = "img/Group-H-Logo.png"/>
-                    </div>
-                    <h3> Teacher Menu </h3>
-                </th>
-            </tr>   
-            <tr>
-                <!-- Search: how to upload things here -->
-                <td class = "firstColumn" > &nbsp; </br> Class List </br> &nbsp; </td>
-                <!-- put the "page" thingy here that changes accordingly when user interacts with the placeholder links. how though???? -->
-                <td rowspan = "5"> (please put the corresponding code here for the respective pages! Maybe it's javascript study that) </td>
-            </tr>
-            <tr>
-                <!-- should be a href link -->
-                <td class = "firstColumn"> &nbsp; </br> Placeholder Link 1 - Settings? </br> &nbsp; </td>
-            </tr>
-            <tr>
-                <!-- should be a href link -->
-                <td class = "firstColumn"> &nbsp; </br> Placeholder Link 2 </br> &nbsp; </td>
-            </tr>
-            <tr>
-                <!-- i think this should be integrated with the mysql database? -->
-                <td class = "firstColumn"> &nbsp; </br> Log Out </br> &nbsp; </td>
-            </tr>
-            <tr>
-                <td class = "firstColumn"> </br></br></br> Group H - Web Application </br> For testing phase only </td>
-            </tr>
-        </table>
+    <?php
+    /*
+        if ($lti -> valid) {
+    ?>
+    <?php
+        unset($_SESSION["username"]);
+        unset($_SESSION["password"]);
+    */
+    ?> 
+        <br/>
+        <div class="con-main">
+            <h1>Attendance System</h1>
+            <h3> Welcome! </h3>
+            <h4> This is the web application of Group H <br/> 
+                designed for the thesis entitled <br/>
+                <i> Development of an Attendance Monitoring System <br/> 
+                with a Portable RFID-based Logging Device. </i>
+            </h4>
+
+            <hr>
+            
+            <div class = "container form-signin"> 
+                <?php
+                    $msg = '';
+                    
+                    if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {               
+                        /*SET USERNAME AND PASSWORD HERE*/   
+                        if ($_POST['username'] == 'admin' &&  $_POST['password'] == '1234') {
+                        $_SESSION['valid'] = true;
+                        $_SESSION['timeout'] = time();
+                        $_SESSION['username'] = 'admin';
+                        
+                        $msg = 'Login Successful!';
+                    }
+                    
+                    else {
+                        $msg = 'Wrong username or password!';
+                    }
+                    }
+                ?>
+            </div>
+
+            <div class = "con-form">      
+                <form class = "form-signin" role = "form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
+                    <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+                    <!-- use "placeholder" for the filler text in boxes -->
+                    <input type = "text" class = "form-control" name = "username" placeholder = "username = admin" required autofocus>
+                    <input type = "password" class = "form-control" bname = "password" placeholder = "password = 1234" required >
+                    <br class = "breakspace"/>
+                    <button class = "buttonLogin" type = "submit" name = "login">Login</button>
+                </form>
+                <p> Are you an administrator? <b> <a href = "admin-login.php" > Log in here! </a> </b></p>
+            </div> 
+        </div>
+    
+    <?php
+    /*
+        } else {
+    ?>
+        <h2>This was not a valid LTI launch</h2>
+        <p>Error message: <?= $lti->message ?></p>
+    <?php
+        }
+    */
+    ?>
     </body>
-</html>
+</html> 
---
+++ teacher-main.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+    <title> Teacher Menu </title>
+    <link type = "text/css" rel = "stylesheet" href = "css/teacher-main-style.css"/>
+    </head>
+    <body>
+        <table class = "tablelogin">
+            <tr>
+                <th>
+                    <!-- temporary icon pls don't judge -->
+                    </br>
+                    <div class = "logo" >
+                        <img src = "img/Group-H-Logo.png"/>
+                    </div>
+                    <h3> Teacher Menu </h3>
+                </th>
+            </tr>   
+            <tr>
+                <!-- Search: how to upload things here -->
+                <td class = "firstColumn" > &nbsp; </br> Class List </br> &nbsp; </td>
+                <!-- put the "page" thingy here that changes accordingly when user interacts with the placeholder links. how though???? -->
+                <td rowspan = "5"> (please put the corresponding code here for the respective pages! Maybe it's javascript study that) </td>
+            </tr>
+            <tr>
+                <!-- should be a href link -->
+                <td class = "firstColumn"> &nbsp; </br> Placeholder Link 1 - Settings? </br> &nbsp; </td>
+            </tr>
+            <tr>
+                <!-- should be a href link -->
+                <td class = "firstColumn"> &nbsp; </br> Placeholder Link 2 </br> &nbsp; </td>
+            </tr>
+            <tr>
+                <!-- i think this should be integrated with the mysql database? -->
+                <td class = "firstColumn"> &nbsp; </br> Log Out </br> &nbsp; </td>
+            </tr>
+            <tr>
+                <td class = "firstColumn"> </br></br></br> Group H - Web Application </br> For testing phase only </td>
+            </tr>
+        </table>
+    </body>
+</html>
