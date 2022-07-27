ca9fa200ec39a260ec3771d755ee9c96df2a1afe
grusnivis <59056214+grusnivis@users.noreply.github.com>
2022-07-28 03:18:36 +0800

    updated with reading .csv files (test .csv file is unwritten) (#1)

--- class-list-upload.php
+++ class-list-upload.php
@@ -32,6 +32,7 @@
             </div>
             <div class = "read-file-con">
                 <h1> Test Output </h1>
+                <p> Before writing .csv file</p>
                 <?php
                     /* 
                     name of csv file,
@@ -40,7 +41,7 @@
                     https://code.tutsplus.com/articles/read-a-csv-to-array-in-php--cms-39471
                     */
                     $row = 1;
-                    $open = fopen("7-20_Teacher1_CPE 3202_g2_22629.csv", "r");
+                    $open = fopen("CPE 3202_g2 S 1-430PM.csv", "r");
                     if($open !== FALSE){
                         echo "<table>\n";
                         while(($data = fgetcsv($open, 1000, ',')) !== FALSE){
@@ -53,7 +54,64 @@
                         echo "</table>\n";
                     }
                     fclose($open);
-                ?>  
+                ?>
+                <br/> <br/>
+                <?php
+                //WRITING CSV FILES
+                $ToBeWritten =[
+                    ['START', '9:00'],
+                    ['END', '10:00'],
+                    ['RFID', 'NAME'],
+                    ['1111', 'student1'],
+                    ['2222', 'student2'],
+                    ['3333', 'student3'],
+                    ['4444', 'student4'],
+                    ['5555', 'student5']
+                ];
+                //prepare file for writing
+                //open .csv file for writing
+                $openFile = fopen("CPE 3202_g2 S 1-430PM.csv", 'w');
+
+                //error when file cannot be opened
+                if ($openFile === false){
+                    die('There was an error opening the file ' . $openFile);
+                }
+
+                //write each ROW at a time to the target file
+                foreach ($ToBeWritten as $row){
+                    fputcsv($openFile, $row);
+                }
+
+                //then you close the .csv file
+                fclose($openFile)
+                ?>
+
+                <?php
+                //FOR TESTING PURPOSES ONLY
+                //after writing to .csv file
+                echo "<p> After writing to .csv file </p>";
+
+                $row = 1;
+                $openTest = fopen("CPE 3202_g2 S 1-430PM.csv", "r");
+                if($openTest !== FALSE){
+                    echo "<table>\n";
+                    while(($dataTest = fgetcsv($openTest, 1000, ',')) !== FALSE){
+                        echo "<tr>";
+                        for ($i = 0; $i < count($dataTest); $i++){
+                            echo "<td><p class = 'instructions'>".$dataTest[$i]." "."</p></td>";
+                        }
+                        echo "</tr>\n";
+                    }
+                    echo "</table>\n";
+                }
+                fclose($openTest);
+                /*
+                 * Do not edit. Tutorials for writing to csv files
+                 * 1) https://stackoverflow.com/questions/17149935/how-to-edit-a-particular-line-of-the-csv-file-using-php
+                 * 2) https://stackoverflow.com/questions/35440228/php-update-specific-row-in-csv-file
+                 * 3) https://stackoverflow.com/questions/39950770/csv-change-one-field-column
+                 */
+                ?>
             </div>
         </form>
     </body>
