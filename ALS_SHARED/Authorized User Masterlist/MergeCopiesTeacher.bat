b7f8f58b927d871256ae6f16e26e4db12cc090b3
grusnivis <kathryn.sigaya@gmail.com>
2023-03-22 08:46:57 +0800

    updated ALS_SHARED folder

---
+++ ALS_SHARED/Authorized User Masterlist/MergeCopiesTeacher.bat
@@ -0,0 +1,6 @@
+echo %~dp0
+pushd %~dp0
+REM "-insert python path here-" "-insert MergeCopiesTeacher.py path here-
+"C:\Users\Amber\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\Amber\PycharmProjects\ALS_SHARED\Authorized User Masterlist\MergeCopiesTeacher.py"
+
+pause
---
+++ ALS_SHARED/Student Masterlist/MergeCopiesStudent.bat
@@ -0,0 +1,6 @@
+echo %~dp0
+pushd %~dp0
+REM "-insert python path here-" "-insert MergeCopiesStudent.py path (to .py) here-
+"C:\Users\Amber\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\Amber\PycharmProjects\ALS_SHARED\Student Masterlist\MergeCopiesStudent.py"
+
+pause
